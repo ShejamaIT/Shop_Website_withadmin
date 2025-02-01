f9cf256d67a085a647f5cfe10ce0b5f1647754fe
ShailiManamperi <shailimanamperi2002@gmail.com>
2025-02-01 18:04:43 +0530

    final upload 02/01

--- backend/Routes/mainRoutes.js
+++ backend/Routes/mainRoutes.js
@@ -8,6 +8,31 @@ const router = express.Router();
 const storage = multer.memoryStorage(); // Store image in memory
 const upload = multer({ storage: storage });
 
+//Get all orders
+router.get("/orders", async (req, res) => {
+    try {
+        // Query the database to fetch all Orders
+        const [orders] = await db.query("SELECT * FROM Orders");
+
+        // If no promotions found, return a 404 status
+        if (orders.length === 0) {
+            return res.status(404).json({ message: "No Orders found" });
+        }
+
+        // Convert the binary image data (LONGBLOB) to Base64
+        // const formattedPromotions = orders.map(order => ({
+        //     id: order.OrID, // Assuming you have an id column
+        //     date: order.date,
+        // }));
+        console.log(orders);
+
+        // Send the formatted promotions as a JSON response
+        return res.status(200).json({ message: "founed" });
+    } catch (error) {
+        console.error("Error fetching promotions:", error.message);
+        return res.status(500).json({ message: "Error fetching promotions" });
+    }
+});
 
 
 
