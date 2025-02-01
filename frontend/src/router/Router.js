2c38c022edd3efc4144f72083705e3e0942a9c0c
ShailiManamperi <shailimanamperi2002@gmail.com>
2025-02-01 17:22:05 +0530

    fixed frist category with dropdown

--- frontend/src/router/Router.js
+++ frontend/src/router/Router.js
@@ -8,6 +8,8 @@ import ProductDetail from "../pages/ProductDetail";
 import Checkout from "../pages/Checkout";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
+import Profile from "../pages/Profile";
+import ProductDisplay from "../components/Ui/ProductDisplay";
 
 
 const Router = () => {
@@ -17,8 +19,10 @@ const Router = () => {
             <Route path='home' element={<Home/>}/>
             <Route path='signup' element={<Signup/>}/>
             <Route path='signin' element={<Signin/>}/>
+            <Route path='profile' element={<Profile/>}/>
             <Route path='shop' element={<Shop/>}/>
             <Route path='cart' element={<Cart/>}/>
+            <Route path="/products/:category" element={<ProductDisplay />} />
             <Route path='productDetail' element={<ProductDetail/>}/>
             <Route path='checkout' element={<Checkout/>}></Route>
             <Route path="/shop/:id" element={<ProductDetail />} />
