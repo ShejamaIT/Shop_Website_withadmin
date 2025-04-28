5e114da2644f8b206a41fe5ba6198f220cc9f56a
ShejamaIT <shejamait@gmail.com>
2025-04-28 12:02:32 +0530

    Update Navbar.jsx

--- admin/src/pages/Navbar.jsx
+++ admin/src/pages/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import '../style/Navbar.css';
+import '../style/Navbar.css'; // assuming you'll put your styles in this file
 import { BiMenu, BiSearch } from 'react-icons/bi';
 
-const Navbar = ({ onNavigate }) => {
+const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="sidebar-button" onClick={() => onNavigate("/dashboard")}>
+            <div className="sidebar-button">
                 <BiMenu className="sidebarBtn" />
                 <span className="dashboard">Dashboard</span>
             </div>
@@ -16,7 +16,7 @@ const Navbar = ({ onNavigate }) => {
             </div>
 
             <div className="profile-details">
-                <img src="admin/src/assets/images/profile.jpeg" alt="Profile" />
+                <img src="/src/assets/images/profile.jpeg" alt="Profile" />
                 <span className="admin_name">Nimna Kaveesha</span>
             </div>
         </nav>
